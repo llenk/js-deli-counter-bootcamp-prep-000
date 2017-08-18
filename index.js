9f187ae5e8849e2ad59470479b626cea079a553b
Ellen Keal <ellenkeal@gmail.com>
2017-08-18 23:57:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ function takeANumber(line, name) {
   line.push(name);
   return ('Welcome, ' + name + '. You are number ' + line.length + ' in line.');
 }
+
+function nowServing(line) {
+  if (length.line === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else {
+    name = line[0];
+    line.shift();
+    return ("Currently serving ." + name + '.');
+  }
+}
