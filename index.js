fc168181bbd658550fa2a61c780c8c90ff941a16
Ellen Keal <ellenkeal@gmail.com>
2017-08-19 00:06:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ function nowServing(line) {
     return ("Currently serving " + name + '.');
   }
 }
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty.";
+  }
+  else {
+    lineList = "The line is currently: ";
+    for (var i = 0; i < line.length; i++) {
+      lineList += (i + 1);
+      lineList += ". ";
+      lineList += name;
+      lineList += ', ';
+    }
+    lineList.slice(0, stringg.length - 2);
+  }
+  return lineList;
+}
