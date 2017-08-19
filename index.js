b5ea1bc2c02a49685417421984a3bf8fb026fdb4
Ellen Keal <ellenkeal@gmail.com>
2017-08-19 00:07:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function currentLine(line) {
     for (var i = 0; i < line.length; i++) {
       lineList += (i + 1);
       lineList += ". ";
-      lineList += name;
+      lineList += lineList[i];
       lineList += ', ';
     }
     lineList.slice(0, lineList.length - 2);
